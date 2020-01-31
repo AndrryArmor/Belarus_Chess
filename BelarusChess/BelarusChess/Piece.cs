957c89e6e55844d7ca163fb59de5bfb3a2bc2d6d
AndrryArmor <andrry1@gmail.com>
2020-01-31 18:43:13 +0200

    Deleted unused code

--- BelarusChess/BelarusChess/Piece.cs
+++ BelarusChess/BelarusChess/Piece.cs
@@ -9,7 +9,7 @@ namespace BelarusChess
     public class Piece
     {
         private Cell cell;
-        protected virtual Move[,] Moves { get; }
+
         public PlayerColor Color { get; }
         public PieceType Type { get; }
         public Cell Cell
@@ -28,31 +28,6 @@ namespace BelarusChess
             Color = color;
             Type = type;
             Cell = cell;
-        }
-
-        public virtual List<Cell> ValidCells(Chessboard chessboard, PlayerColor playerColor)
-        {
-            var validCells = new List<Cell>();
-
-            /// Finds valid moves for piece
-            for (int i = 0; i < Moves.GetLength(0); i++)
-            {
-                for (int j = 0; j < Moves.GetLength(1); j++)
-                {
-                    Cell newCell = Cell.Create(Cell.Row + Moves[i, j].Rows, Cell.Col + Moves[i, j].Cols);
-
-                    /// If cell not valid or contains friendly piece
-                    if (newCell == null || (chessboard[newCell] != null && chessboard[newCell].Color == playerColor))
-                        break;
-
-                    validCells.Add(newCell);
-
-                    /// If cell contains opponent's piece or it stays on throne (except prince)
-                    if (chessboard[newCell] != null || (newCell.Row == 4 && newCell.Col == 4 && Type != PieceType.Prince))
-                        break;
-                }
-            }
-            return validCells;
         }
     }
 }
